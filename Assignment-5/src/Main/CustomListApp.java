2c9771676bc75798626411632d92e1c3b86c9528
Rini <92247940+RasmusPfeiffer@users.noreply.github.com>
2022-03-31 19:56:55 +0200

    Update CustomListApp.java

--- Assignment-5/src/Main/CustomListApp.java
+++ Assignment-5/src/Main/CustomListApp.java
@@ -7,11 +7,12 @@ public class CustomListApp {
 	public static void main(String[] args) {
 
 		CustomArrayList<String> elements = new CustomArrayList<>();
-		for (int i = 0; i < 54; i++) {
+		for (int i = 0; i < 81; i++) {
 			elements.add("Element " + i);
 		}
 		for (int i = 0; i < elements.getSize(); i++) {
 			System.out.println(elements.get(i));
 		}
+		System.out.println(elements.getSize());
 	}
 }
